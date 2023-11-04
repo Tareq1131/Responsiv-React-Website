936e8cb76e81726b2f279c62ab6245dad15445d2
Tareq1131 <tareqislam076@gmail.com>
2023-11-05 00:41:59 +0600

    Project setup and router setup

---
+++ src/firease/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApnIZ2XmCmYpUVrWA3YCUsauaFAdi9PNU",
+  authDomain: "simple-firebase-f4407.firebaseapp.com",
+  projectId: "simple-firebase-f4407",
+  storageBucket: "simple-firebase-f4407.appspot.com",
+  messagingSenderId: "387361091853",
+  appId: "1:387361091853:web:df33c935bd280370db0044"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Header from '../pages/shared/Header/Header';
+import Footer from '../pages/shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
