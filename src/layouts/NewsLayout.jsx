5f8258cbb1c33316a132e3d36a59acc75e89be1a
Tareq1131 <tareqislam076@gmail.com>
2023-11-06 00:09:53 +0600

    Display News Details and editors insights

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -1,29 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 import Header from "../pages/shared/Header/Header";
 import Footer from "../pages/shared/Footer/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import LeftNav from "../pages/shared/leftNav/LeftNav";
 import RightNav from "../pages/shared/rightNav/RightNav";
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const NewsLayout = () => {
-    return (
-        <div>
-            <Header></Header>
+  return (
+    <div>
+      <Header></Header>
       <Container>
-        
         <Row>
-          <Col lg = {9}>
-          <Outlet />
+          <Col lg={9}>
+            <Outlet />
           </Col>
-          <Col lg = {3}>
+          <Col lg={3}>
             <RightNav></RightNav>
           </Col>
         </Row>
       </Container>
       <Footer></Footer>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default NewsLayout;
